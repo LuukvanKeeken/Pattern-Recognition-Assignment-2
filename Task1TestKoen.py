f5df61cee3c6da865d6aa417c09ad799cd2ced78
kvanschuijlenburg <kvanschuijlenburg@gmail.com>
2022-10-18 11:46:20 +0200

    including class weights

--- Task1TestKoen.py
+++ Task1TestKoen.py
@@ -72,6 +72,12 @@ class Pipeline:
         samplesPerClass = [0]*len(self.labelsDict)
         for label in self.preProcessedLabels:
             samplesPerClass[label] +=1
+        leastItems = np.min(samplesPerClass)
+
+        self.classWeights={}
+        for index, i in enumerate(samplesPerClass):
+            weight = leastItems/i
+            self.classWeights[index]=weight
         
         print("The data set has "+ str(len(self.preProcessedData))+ " samples divided over " + str(len(self.labelsDict)))
         print("For labels " + str(self.labelsDict) + ",the number of classes are: " + str(samplesPerClass))
@@ -124,49 +130,69 @@ class Pipeline:
             plt.savefig(f"Figures{os.sep}PCA")
         return reducedDimensionsData
     
-
-
-    def validation(self, data, labels, kFolds):
+    def validation(self, data, labels):
         model = Model()
         # TODO: for training cost sensitive error function becuase number of samples per class
-        foldSize = round(len(data)/kFolds)
+        
         accuracies = []
-        for fold in range(kFolds):
-            print("Fold " + str(fold+1))
-            # Split the data in training and validation data
-            valSetIdx = np.arange(fold*foldSize,(fold+1)*foldSize, 1, int)
-            trainX = np.delete(data, valSetIdx,axis=0)
-            trainY = np.delete(labels, valSetIdx,axis=0)
-            valX = data[valSetIdx]
-            valY = labels[valSetIdx]
+        for fold in range(len(labels)):
+            if fold % 100 == 0:
+                print("Fold " + str(fold+1))
+            trainX = np.delete(data,fold,axis=0)
+            trainY = np.delete(labels,fold,axis=0)
+            valX = data[[fold]]
+            valY = labels[[fold]]
             
             # Train and validate the model
-            model.train(trainX, trainY)
+            model.train(trainX, trainY, self.classWeights)
             accuracies.append(model.test(valX, valY))
 
         accuracy = np.mean(accuracies)
         print("Done, average accuracy is: " + str(round(accuracy,3))+"%")
         return accuracy
+
+    # def validation(self, data, labels, kFolds):
+    #     model = Model()
+    #     # TODO: for training cost sensitive error function becuase number of samples per class
+    #     kFolds = 800
+    #     foldSize = 1# round(len(data)/kFolds)
+    #     accuracies = []
+    #     for fold in range(kFolds):
+    #         print("Fold " + str(fold+1))
+    #         # Split the data in training and validation data
+    #         valSetIdx = np.arange(fold*foldSize,(fold+1)*foldSize, 1, int)
+    #         trainX = np.delete(data, valSetIdx,axis=0)
+    #         trainY = np.delete(labels, valSetIdx,axis=0)
+    #         valX = data[valSetIdx]
+    #         valY = labels[valSetIdx]
+            
+    #         # Train and validate the model
+    #         model.train(trainX, trainY)
+    #         accuracies.append(model.test(valX, valY))
+
+    #     accuracy = np.mean(accuracies)
+    #     print("Done, average accuracy is: " + str(round(accuracy,3))+"%")
+    #     return accuracy
         
 
 class Model:
     def __init__(self):
         self.regressionModel = LogisticRegression(random_state=16)
 
-    def train(self, trainX, trainY):
-        self.regressionModel = LogisticRegression(random_state=16, max_iter=10000)
+    def train(self, trainX, trainY, classWeights):
+        self.regressionModel = LogisticRegression(random_state=16, max_iter=10000, class_weight=classWeights)
         self.regressionModel.fit(trainX, trainY)
 
     def test(self, testX, testY):
         predictY = self.regressionModel.predict(testX)
-        cnf_matrix = metrics.confusion_matrix(testY, predictY)   
-        print(cnf_matrix)
-
-        correct = 0
-        for test, predict in zip(testY, predictY):
-            if (test==predict):
-                correct +=1
-        accuracy = correct/len(testY)
+        #cnf_matrix = metrics.confusion_matrix(testY, predictY)   
+        #print(cnf_matrix)
+        accuracy = testY==predictY
+        # correct = 0
+        # for test, predict in zip(testY, predictY):
+        #     if (test==predict):
+        #         correct +=1
+        # accuracy = correct/len(testY)
         return accuracy
 
 
@@ -184,7 +210,7 @@ if __name__=="__main__":
     for dimension in range(1,maxNumberOfDimensions+1):
         print("Grid search "+ str(dimension))
         dataReduced = pipeline.dimensionReduction(pipeline.dataX, pipeline.dataY, dimension)
-        accuracies.append(pipeline.validation(dataReduced, pipeline.dataY, kFolds=10))
+        accuracies.append(pipeline.validation(dataReduced, pipeline.dataY))#, kFolds=10))
 
     fig = plt.figure(figsize = (8,8))
     plt.plot(range(1,len(accuracies)+1), accuracies)
